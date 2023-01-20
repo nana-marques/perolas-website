b9f5a033a09b4b411cd90f0cd11fd5e68888038a
nana-marques <annamarques16@hotmail.com>
2023-01-20 16:00:20 -0300

    teste 2

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -6,6 +6,7 @@
     <?php
         echo link_tag('assets/css/custom.css', 'stylesheet');
         //echo meta('description', '');
+        //teste
     ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
