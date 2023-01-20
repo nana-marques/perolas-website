b20ce899c2e25e65da108e9b508a141144a945a6
Ana Marques <ana.marques@crfsp.org.br>
2023-01-20 15:54:00 -0300

    teste

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -10,7 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
-    <link rel="icon" href="assets/img/favicon.png" sizes="35x35"/> 
+    <!-- <link rel="icon" href="assets/img/favicon.png" sizes="35x35"/>  -->
     <title>Pérolas Website</title>
     <style>
         html:focus-within{
