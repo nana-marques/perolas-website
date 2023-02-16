692f5cd1026dd79a434ad62ce0788a4b01f3b4d2
nana-marques <annamarques16@hotmail.com>
2023-02-16 13:48:35 -0300

    fix - responsividade da navbar

--- application/views/static/header.php
+++ application/views/static/header.php
@@ -2,17 +2,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pérolas Website</title>
+    
+
+    <!-- IMPORT BOOTSTRAP SCRIPTS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    
     <?php
         echo link_tag('assets/css/custom.css', 'stylesheet');
         //echo meta('description', '');
         //teste
-    ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+        ?>
     <link rel="icon" href="assets/img/favicon.png" sizes="48x48"/> 
-    <title>Pérolas Website</title>
     <style>
         html:focus-within{
             scroll-behavior: smooth !important;
@@ -20,7 +26,7 @@
         html{
             scroll-behavior: smooth !important;
         }
-
+        
         .text-white{
             color: #f3f3f3 !important;
         }
@@ -29,76 +35,87 @@
             background-color: #F4AFBD;
             color: #000 !important;
         }
-
+        
         body{
             background-color: #f7f7f7;
             color: black;
-
+            
         }
-
+        
         .bg-secondary{
             /*color: #534d56;*/
             background-color: #3C2439!important;
         }
-
+        
         .nav-link{
             color: #fff !important; 
             font-weight: normal;
         }
-
+        
         .show{
             color: #f7f7f7 !important;
-
+            
         }
-
+        
         .btn-primary{
             background-color: #F4AFBD;
             border-color: #F4AFBD;
         }
-
+        
         .btn-primary:hover{
             background-color: #E55175;
             border-color: #E55175;
         }
-
+        
         .btn-dark{
             color: #E55175 !important;
         }
-
+        
         .btn-dark:hover{
             color: #140C13 !important;
         }
-
+        
         .btn-secondary{
             background-color: #f39cbc !important;
             border-color: #f39cbc !important;
         }
-
+        
         footer a{
             color: #E55175;
             text-decoration: none;
         }
-
+        
     </style>
+
 </head>
-<header>
+<body>
+    
+    
     <nav class="navbar navbar-expand-md navbar-light fixed-top">
-        <a class="navbar-brand" href="http://localhost/Ana/perolas-website/homepage"><img src="assets/img/logo-softZoom.png" alt="Pérolas Logo" style="width: 4vw; margin-left: 10px;"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="http://localhost/Ana/perolas-website/homepage"><img src="assets/img/logo-softZoom.png" alt="Pérolas Logo" style="width: 3em; margin-left: 10px;"></a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ml-auto flex-nowrap navbar-right">
-                <a class="nav-item nav-link mr-2" href="homepage" style="color: #f7f7f7; font-weight: 500;">Home <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link mr-2" href="http://localhost/Ana/perolas-website/homepage#produtos" style="color: #f7f7f7; font-weight: 500;">Produtos</a>
-                <a class="nav-item nav-link mr-2" href="about" style="color: #f7f7f7; font-weight: 500;">Sobre Nós</a>
-                <a class="nav-item nav-link mr-2" href="contatar" style="color: #f7f7f7; font-weight: 500;">Contato</a>
-                <!-- <div class="custom-control custom-switch d-flex align-items-center">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup" style>
+            <ul class="navbar-nav navbar-right ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link mr-2" href="homepage" style="color: #f7f7f7; font-weight: 500;">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-2" href="http://localhost/Ana/perolas-website/homepage#produtos" style="color: #f7f7f7; font-weight: 500;">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-2" href="about" style="color: #f7f7f7; font-weight: 500;">Sobre Nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link mr-2" href="contatar" style="color: #f7f7f7; font-weight: 500;">Contato</a>
+                </li>
+                <!-- <ul class="custom-control custom-switch d-flex align-items-center">
                     <input type="checkbox" class="custom-control-input" name="dark-mode" id="selector">
                     <label for="selector" class="custom-control-label text-white">Dark Mode</label>
-                </div> -->
-            </div>
+                </ul> -->
+            </ul>
         </div>
     </nav>
     <!-- <div>
@@ -107,4 +124,5 @@
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div> -->
-</header>
+    
+</body>
